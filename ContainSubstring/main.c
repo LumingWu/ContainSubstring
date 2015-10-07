589d77abe6bcf8f091779b9c556ad1f1bfbb78e5
Luming Wu <reallumingwu@gmail.com>
2015-10-07 18:38:59 -0400

    This is an empty project commit.

---
+++ ContainSubstring/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: umola_000
+ *
+ * Created on October 7, 2015, 6:36 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
