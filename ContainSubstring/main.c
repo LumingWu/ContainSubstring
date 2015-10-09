fae78323a6e6a6ebb4967dc6e1720d7a0af2d025
Luming Wu <reallumingwu@gmail.com>
2015-10-09 17:20:42 -0400

    Yea, the else statement...

--- ContainSubstring/main.c
+++ ContainSubstring/main.c
@@ -7,9 +7,10 @@
  **/
 int main(int argc, char** argv) {
     char* string1 = "saskdjaskdjaskdjasdmasos";
-    char* string2 = "zd";
+    char* string2 = "zsasad";
     printf("The first string is: %s\nThe second string is: %s\nThe answer is %s\n", string1, string2, 
-            ContainSubstring_Brute(string1,string2)? "true" : "false");
+            ContainSubstring_Dummy(string1,string2)? "true" : "false");
+    make_DFA(string2);
     return (EXIT_SUCCESS);
 }
 
