31b44dc8b609ce1375e6d9e6e70a4578f92b7837
Luming Wu <reallumingwu@gmail.com>
2015-10-10 09:18:41 -0400

    Many changes: 	(1) The function takes in char* substring. When substring is n length, there are n*n literals I have to create. If I can get the pointer of the substring, I can keep using that address to compare instead of constantly allocating space in the C hash table. (I am not sure how C stores string literals, please educate me) 	(2) Use while loop instead of if(){}else if(){}else if{}... Again, don't want to create too much string literals for memcmp(). 	(3) I added j as a temp counter for doing the checking. I think comparing must be done from the best result to the worst result. So while loop does the same thing and perform better.
    PS: The string in the printf() can combine instead of having multiple printf()'s. I commit right now so that when I need to adjust something. I can start here. Because I don't want to look through a long string literal to know which part I am changing. The DFA saved a little bit of time, I am not sure if it is better is larger scale(substring is n length).

--- ContainSubstring/main.c
+++ ContainSubstring/main.c
@@ -6,11 +6,13 @@
  * This main is just a format for testing the method.
  **/
 int main(int argc, char** argv) {
-    char* string1 = "saskdjaskdjaskdjasdmasos";
-    char* string2 = "zsasad";
-    printf("The first string is: %s\nThe second string is: %s\nThe answer is %s\n", string1, string2, 
+    char* string1 = "saskdsskdjaskdjasdmasos";
+    char* string2 = "dss";
+    //printf("%s\n", memcmp(string1 + 4,"dss", 3) == 0? "true" : "false");
+    //printf("The first string is: %s\nThe second string is: %s\nThe answer is %s\n", string1, string2, 
             ContainSubstring_Dummy(string1,string2)? "true" : "false");
-    make_DFA(string2);
+    //make_DFA(string2);
+    //printf("%s\n", GIVE_A_NAME(string1, string2)? "true" : "false");
     return (EXIT_SUCCESS);
 }
 
